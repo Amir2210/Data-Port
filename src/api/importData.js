fdf5713e6c6e85e8dc5e9d56f5c64ef45b69c51f
Amir Yankolovich <amiryankolovich2000@gmail.com>
2025-04-22 15:16:09 +0300

    Add all the columns

--- src/api/importData.js
+++ src/api/importData.js
@@ -2,12 +2,12 @@ export const importData = {
   fetchData
 }
 
-async function fetchData() {
+async function fetchData(year) {
   const url = 'https://data.gov.il/api/3/action/datastore_search?resource_id=80c1e38e-06b9-4a67-b2a4-cc1a76374ee9&'
   try {
     const res = await fetch(url)
     if (!res.ok) {
-      throw new Error("Failed to fetch data")
+      throw new Error("Failed to fetch")
     }
     const json = await res.json()
     return json.result.records
--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -6,6 +6,7 @@ import { Box, Typography } from '@mui/material';
 export function DataTable() {
   const [data, setData] = useState([])
   const [error, setError] = useState("")
+  const [year, setYear] = useState(2025)
 
   useEffect(() => {
     async function getData() {
@@ -20,28 +21,52 @@ export function DataTable() {
   }, [])
 
   const columns = [
-    { field: '_id', headerName: 'ID', width: 70 },
-    { field: 'Year', headerName: 'שנה', width: 100 },
-    { field: 'Month', headerName: 'חודש', width: 100 },
+    { field: 'Year', headerName: 'שנה', width: 90 },
+    { field: 'Month', headerName: 'חודש', width: 90 },
     { field: 'Origin_Country', headerName: 'מדינת מקור', width: 150 },
-    { field: 'CustomsItem_8_Digits', headerName: 'פריט מכס (8 ספרות)', width: 180 },
+    { field: 'CustomsItem_2_Digits', headerName: 'פריט מכס (2 ספרות)', width: 150 },
+    { field: 'CustomsItem_8_Digits', headerName: 'פריט מכס (8 ספרות)', width: 150 },
+    { field: 'Exempt_CustomsItem', headerName: 'פטור ממכס', width: 130 },
+    { field: 'CustomsHouse', headerName: 'בית מכס', width: 120 },
     { field: 'Quantity', headerName: 'כמות', width: 120 },
-    { field: 'Quantity_MeasurementUnitName', headerName: 'יחידת מידה', width: 150 },
-    { field: 'CurrencyCode', headerName: 'מטבע', width: 100 },
-    { field: 'NISCurrencyAmount', headerName: 'שווי בש"ח', width: 150 },
+    { field: 'Quantity_MeasurementUnitID', headerName: 'קוד יחידת מידה', width: 140 },
+    { field: 'Quantity_MeasurementUnitName', headerName: 'שם יחידת מידה', width: 150 },
+    { field: 'TradeAgreementName', headerName: 'הסכם סחר', width: 180 },
+    { field: 'TermsOfSale', headerName: 'תנאי מכירה', width: 150 },
+    { field: 'IsTradeAgreementWithQuota', headerName: 'האם הסכם סחר עם מכסה', width: 200 },
+    { field: 'NISCurrencyAmount', headerName: 'שווי בש"ח', width: 130 },
+    { field: 'CurrencyCode', headerName: 'קוד מטבע', width: 100 },
+    { field: 'AutonomyTypeID', headerName: 'קוד אוטונומיה', width: 130 },
+    { field: 'AutonomyType', headerName: 'סוג אוטונומיה', width: 150 },
+    { field: 'GovernmentProcedureTypeName', headerName: 'תהליך ממשלתי (עברית)', width: 180 },
+    { field: 'GovernmentProcedureTypeEnglishName', headerName: 'תהליך ממשלתי (אנגלית)', width: 200 },
+    { field: 'IsPreferenceDocument', headerName: 'יש מסמך העדפה', width: 160 },
+    { field: 'GeneralCustomsTax', headerName: 'מס מכס כללי', width: 150 },
+    { field: 'PurchaseTax', headerName: 'מס קניה', width: 120 },
+    { field: 'VAT', headerName: 'מע״מ', width: 100 },
   ];
 
   return (
-    <Box sx={{ height: 600, width: '100%', p: 2 }}>
-      <Typography variant="h5" gutterBottom>יבוא לישראל</Typography>
-      {error && <Typography color="error">{error}</Typography>}
-      <DataGrid
-        rows={data}
-        columns={columns}
-        getRowId={(row) => row._id}
-        pageSize={10}
-        rowsPerPageOptions={[10, 25, 50]}
-      />
-    </Box>
-  );
+    <div>
+      <h1 className='text-4xl text-stone-800 font-bold font-mono text-center pt-2'>יבוא לישראל לשנת <span>{year}</span></h1>
+      <Box sx={{ height: 600, width: '100%', p: 2 }}>
+        <div className='flex gap-10'>
+          <select name='year' className="" onChange={(ev) => setYear(ev.target.value)}>
+            <option>2025</option>
+            <option>2024</option>
+            <option>2023</option>
+            <option>2022</option>
+          </select>
+        </div>
+        {error && <Typography color="error">{error}</Typography>}
+        <DataGrid
+          rows={data}
+          columns={columns}
+          getRowId={(row) => row._id}
+          pageSize={10}
+          rowsPerPageOptions={[10, 25, 50]}
+        />
+      </Box>
+    </div>
+  )
 }

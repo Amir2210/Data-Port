de7bd7c014e0b6bf1340bbddc1a6c276dac3a59c
Amir Yankolovich <amiryankolovich2000@gmail.com>
2025-04-28 10:04:17 +0300

    Remove unneccesary columns

--- src/components/statistics/Top5ExpensiveImport.jsx
+++ src/components/statistics/Top5ExpensiveImport.jsx
@@ -1,5 +1,6 @@
 import { Box } from '@mui/material'
 import { DataGrid } from '@mui/x-data-grid'
+import numeral from 'numeral'
 export function Top5ExpensiveImport({ top5ExpensiveImports, year }) {
 
   const columns = [
@@ -7,43 +8,25 @@ export function Top5ExpensiveImport({ top5ExpensiveImports, year }) {
     { field: 'Year', headerName: 'שנה', width: 110 },
     { field: 'Month', headerName: 'חודש', width: 110 },
     { field: 'Origin_Country', headerName: 'מדינת מקור', width: 160 },
-    { field: 'CustomsItem_2_Digits', headerName: 'פריט מכס (2 ספרות)', width: 230 },
-    { field: 'CustomsItem_8_Digits', headerName: 'פריט מכס (8 ספרות)', width: 230 },
-    {
-      field: 'Exempt_CustomsItem', headerName: 'פטור ממכס', width: 160, valueFormatter: (params) => {
-        if (!params) return ''
-        return (+params).toLocaleString()
-      }
-    },
-    { field: 'CustomsHouse', headerName: 'בית מכס', width: 200 },
     {
       field: 'Quantity', headerName: 'כמות', width: 120, valueFormatter: (params) => {
         if (!params) return ''
         return (+params).toLocaleString()
       }
     },
-    { field: 'Quantity_MeasurementUnitID', headerName: 'קוד יחידת מידה', width: 230 },
-    { field: 'Quantity_MeasurementUnitName', headerName: 'שם יחידת מידה', width: 230 },
-    { field: 'TermsOfSale', headerName: 'תנאי מכירה', width: 160 },
     {
       field: 'NISCurrencyAmount', headerName: 'שווי בש"ח', width: 150, valueFormatter: (params) => {
         if (!params) return ''
-        return (+params).toLocaleString()
+        return numeral(+params).format('0.[0]aa') + '₪'
       }
     },
     { field: 'CurrencyCode', headerName: 'קוד מטבע', width: 150 },
-    {
-      field: 'VAT', headerName: 'מע״מ', width: 130, valueFormatter: (params) => {
-        if (!params) return ''
-        return (+params).toLocaleString()
-      }
-    },
   ]
   return (
-    <div>
+    <div className='flex flex-col items-center'>
       <h2 className='text-4xl text-center text-blue-600 font-bold py-2 underline'>נתונים נוספים וחריגים</h2>
-      <h2 className='text-xl text-stone-800 font-bold text-center mt-4' dir='rtl'>5 היבואים עם השווי היקר ביותר בשנת {year}</h2>
-      <Box sx={{ height: 560, width: '100%', p: 2 }}>
+      <h2 className='text-xl text-stone-800 font-bold text-center mt-4' dir='rtl'>5 היבואנים עם השווי היקר ביותר בשנת {year}</h2>
+      <Box sx={{ height: 560, width: '60%', p: 2 }}>
         <DataGrid
 
           sx={{
